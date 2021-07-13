41e093327c4c72acba5968519ef5708d63b8edb7
yeonjin1357 <yeonjin1357@naver.com>
2021-07-13 13:43:27 +0900

    0712_JustShop

---
+++ script/script.js
@@ -0,0 +1,90 @@
+/* eslint-disable */
+
+$(function () {
+
+    $(".goods1").click(function () {
+        $("#layerBG").css({
+            "display": "block"
+        });
+
+
+    })
+    
+    $(".goods2").click(function () {
+        $("#layerGoods2").css({
+            "display": "block"
+        });
+
+
+    })
+    
+    $("#btnArea button").click(function () {
+        $("#layerGoods2").css({
+            "display": "none"
+        });
+    })
+
+
+    $("#btnArea button").click(function () {
+        $("#layerBG").css({
+            "display": "none"
+        });
+    })
+    
+    $(".goods3").click(function () {
+        $("#layerGoods3").css({
+            "display": "block"
+        });
+
+
+    })
+    
+    $("#btnArea button").click(function () {
+        $("#layerGoods3").css({
+            "display": "none"
+        });
+    })
+    
+    $(".goods4").click(function () {
+        $("#layerGoods4").css({
+            "display": "block"
+        });
+
+
+    })
+    
+    $("#btnArea button").click(function () {
+        $("#layerGoods4").css({
+            "display": "none"
+        });
+    })
+    
+    $(".goods5").click(function () {
+        $("#layerGoods5").css({
+            "display": "block"
+        });
+
+
+    })
+    
+    $("#btnArea button").click(function () {
+        $("#layerGoods5").css({
+            "display": "none"
+        });
+    })
+    
+    $(".goods6").click(function () {
+        $("#layerGoods6").css({
+            "display": "block"
+        });
+
+
+    })
+    
+    $("#btnArea button").click(function () {
+        $("#layerGoods6").css({
+            "display": "none"
+        });
+    })
+
+});
